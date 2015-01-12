edfa20281deb4a15b49642acc954fcb644b3d298
mnmnc <chooseright+git@gmail.com>
2015-01-12 23:43:19 +0100

    Maze solving with Tremaux's Algorithm

--- tremaux.py
+++ tremaux.py
@@ -1,12 +1,16 @@
 from PIL import Image
 import random
+from time import sleep
+import time
 
 def create_image(size):
 	im = Image.new("RGB", (size, size), color=(255,255,255))
 	return im
 
+
 def save_image(image, filename):
 	image.save(filename, "PNG")
+
 
 def print_graph_as_x_matrix(graph):
 	for row in graph:
@@ -21,6 +25,7 @@ def print_graph_as_x_matrix(graph):
 				print("  ",end="")
 		print(" ")
 
+
 def print_graph_special(graph, point_x, point_y):
 	for i in range(len(graph)):
 		for j in range(len(graph[i])):
@@ -32,6 +37,7 @@ def print_graph_special(graph, point_x, point_y):
 				print("  ",end="")
 		print(" ")
 
+
 def parse_cords_file(filename):
 	parsed_cords = []
 	with open(filename,"r") as input:
@@ -41,12 +47,14 @@ def parse_cords_file(filename):
 			parsed_cords.append(cords_list)
 	return parsed_cords
 
+
 def find_biggest(parsed_cords):
 	biggest = -1
 	for node in parsed_cords:
 		if node[0] > biggest:
 			biggest = node[0]
 	return biggest
+
 
 def create_empty_graph(biggest):
 	graph = []
@@ -56,6 +64,7 @@ def create_empty_graph(biggest):
 			ys.append(0)
 		graph.append(ys)
 	return graph
+
 
 def set_graph_values(graph, parsed_cords):
 	for coord in parsed_cords:
@@ -71,6 +80,7 @@ def set_graph_values(graph, parsed_cords):
 			graph[coord[0]+1][coord[1]] = 1
 		if coord[2] == 3:
 			graph[coord[0]][coord[1]] = 1
+
 
 def fix_graph_frames(graph):
 	graph_trans = []
@@ -89,6 +99,7 @@ def fix_graph_frames(graph):
 				graph_trans[i][j] = 1
 
 	return graph_trans
+
 
 def count_neighbours_with_value(graph, cords, value):
 	count = 0
@@ -169,13 +180,31 @@ def draw_graph_on_image(image, graph):
 
 def main():
 
-	# VARIABLES
+	# FILE VARIABLES
 	input_file = "labirynth_cords.txt"
 	output_file = "maze2.png"
 	small_input_file = "small_maze.txt"
 
+	# PSEUDO-COLORS VALUES
+	wall_value = 1
+	unvisited_value = 0
+	visited_once = 0.01
+	visited_twice = 0.02
+
+	# ENTRY AND EXIT
+	begin = (1, 1)
+	end = (255, 255)
+
+	# OTHER VARIABLES
+	iteration = 0
+	exit_found = False
+	debug = False
+
+	# SET CURRENT POSITION
+	current_position = (begin[0], begin[1])
+
 	# PARSING FILE
-	parsed_cords = parse_cords_file(small_input_file)
+	parsed_cords = parse_cords_file(input_file)
 
 	# CORRECTING
 	biggest = find_biggest(parsed_cords)
@@ -190,50 +219,43 @@ def main():
 	graph = fix_graph_frames(graph)
 
 	# PRINT RESULT
-	print_graph_as_x_matrix(graph)
+	# print_graph_as_x_matrix(graph)
 
-	# VARIABLES
-	wall_value = 1
-	unvisited_value = 0
-	visited_once = 0.01
-	visited_twice = 0.02
-	begin_x = 1
-	begin_y = 1
-	end_x = 31
-	end_y = 31
-	begin = (begin_x, begin_y)
-	end = (end_x, end_y)
-	current_position = (begin[0], begin[1])
+	# CREATING IMAGE OUTPUT
+	im = create_image(len(graph)*2)
 
-	debug_counter = 0
-	exit_found = False
+	start_time = time.time()
+	# FINDING PATH
+	while exit_found == False:
+		iteration += 1
 
-
-	while debug_counter < 451:
-		print(" ")
-		print(current_position)
-
+		# CHECK IF MR TREMAUX REACHED THE END
 		if current_position == end:
-			debug_counter == 999
-			print("Finished")
+			print("Time spent solving:", time.time() - start_time)
+			exit_found = True
+			#print_graph_as_x_matrix(graph)
+			draw_graph_on_image(im, graph)
+			save_image(im, output_file)
 			break
 
-		debug_counter += 1
-
+		# CHECK NUMBER OF WALLS AROUND
 		count = count_neighbours_with_value(graph,current_position, wall_value)
-		print("count:", count)
 
-
+		# IF MR TREMAUX HAS ONLY ONE WAY (THE WAY BACK)
 		if count > 2 and current_position != begin and current_position != end:
 			graph[current_position[0]][current_position[1]] += visited_twice
 		else:
+			# ELSE, IT'S A NORMAL AFTERNOON WALK
 			graph[current_position[0]][current_position[1]] += visited_once
 
+		# GET ALL POSSIBLE MOVES IN NEXT STEP
 		next_moves = get_available_moves(graph,current_position, [unvisited_value, visited_once])
 
+		# IF WE ARE ON A CROSSROADS, PREVENT FROM MARKING IT AS DEAD END
 		if len(next_moves) > 1:
 			graph[current_position[0]][current_position[1]] = visited_once
 
+		# SPLIT NEXT MOVES BASED ON PREVIOUS VISITS
 		visited =[]
 		unvisited = []
 		for move in next_moves:
@@ -242,6 +264,9 @@ def main():
 			if move[2] == visited_once:
 				visited.append(move)
 
+		# MOVE CHOICE
+		# UNVISITED PATHS HAVE HIGHER PRIORITY
+		# IF MULTIPLE STEPS ARE AVAILABLE - CHOOSE RANDOMLY
 		selected_move = []
 		if len(unvisited) > 0:
 			if len(unvisited) > 1:
@@ -256,70 +281,16 @@ def main():
 			else:
 				selected_move = visited[0]
 
+		# TAKE THE STEP
 		current_position = (selected_move[0], selected_move[1])
 
-
-		print_graph_as_x_matrix(graph)
-
-
-
-
-
-
-		# if current_position == end:
-		# 	exit_found = True
-		# 	print_graph_as_x_matrix(graph)
-		# 	im = create_image(len(graph)*2)
-		# 	draw_graph_on_image(im, graph)
-		# 	save_image(im, output_file)
-		# 	print("exit found")
-		# elif count > 2 and current_position != begin and current_position != end:
-		# 	graph[current_position[0]][current_position[1]] += visited_twice
-		# else:
-		# 	graph[current_position[0]][current_position[1]] += visited_once
-		#
-		# next_moves = get_available_moves(graph,current_position, [unvisited_value, visited_once])
-		#
-		# if len(next_moves) > 1:
-		# 	graph[current_position[0]][current_position[1]] = visited_once
-		#
-		# print_graph_as_x_matrix(graph)
-		#
-		# unvisited_paths = []
-		# visited_paths = []
-		#
-		# for move in next_moves:
-		# 	print(move)
-		# exit_found = True
-
-		# lowest_value = 9
-		# selected_moves = []
-		# selected_move = []
-		# for move in next_moves:
-		# 	if move[2] <= lowest_value:
-		# 		lowest_value = move[2]
-		#
-		# equal_moves = 0
-		# for move in next_moves:
-		# 	if move[2] == lowest_value:
-		# 		equal_moves += 1
-		#
-		# if equal_moves > 1:
-		# 	equal_moves_list = []
-		# 	for move in next_moves:
-		# 		if move[2] == lowest_value:
-		# 			equal_moves_list.append(move)
-		# 	random_number = random.randint(0, len(equal_moves_list)-1)
-		# 	print("Random choice:",  random_number)
-		# 	selected_move = equal_moves_list[random_number]
-
-
-		#current_position = (selected_move[0], selected_move[1])
-
-		#print_graph_as_x_matrix(graph)
-
-
-
+		# OPTIONAL LIVE UPDATE
+		if debug and iteration % 100 == 0:
+			try:
+				draw_graph_on_image(im, graph)
+				save_image(im, output_file)
+			except:
+				pass
 
 
 if __name__ == "__main__":
